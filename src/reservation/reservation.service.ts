8a1c4e2352907ec685bb82c944cadd19ddd0e05e
minimini1212 <zjonyv12@naver.com>
2024-01-03 19:25:44 +0900

    feat: 트렌젝션 구현

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -7,7 +7,7 @@ import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Reservation } from './entities/reservation.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Schedule } from 'src/concert/entities/schedule.entity';
 import { User } from 'src/user/entities/user.entity';
 
@@ -20,53 +20,62 @@ export class ReservationService {
     private readonly scheduleRepository: Repository<Schedule>,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    // 트렌젝션을 위한 초석
+    private readonly dataSource: DataSource,
   ) {}
 
   // 예매하기 API
   async create(createReservationDto: CreateReservationDto, user) {
-    const userId = user.id;
-    // 해당유저의 포인트
-    const userPoint = user.point;
-    const { scheduleId } = createReservationDto;
-    const concert = await this.findSeat(scheduleId);
-    const concertPrice = concert.concert.price;
+    // 트렌젝션을 위한 초석
+    const reservation = await this.dataSource.transaction(async (manager) => {
+      const userId = user.id;
+      // 해당유저의 포인트
+      const userPoint = user.point;
+      const { scheduleId } = createReservationDto;
+      const concert = await this.findSeat(scheduleId);
+      const concertPrice = concert.concert.price;
 
-    // 그 공연날짜의 예매 가능 좌석 수 가져오기
-    const seat = concert.availableSeat;
+      // 그 공연날짜의 예매 가능 좌석 수 가져오기
+      const seat = concert.availableSeat;
 
-    if (seat < 1) {
-      throw new ForbiddenException('좌석이 없어서 예매가 불가능합니다.');
-    }
-    //예약을 하면 예매가능 좌석수를 줄여야 한다.
-    const remainingSeat = seat - 1;
+      if (seat < 1) {
+        throw new ForbiddenException('좌석이 없어서 예매가 불가능합니다.');
+      }
+      //예약을 하면 예매가능 좌석수를 줄여야 한다.
+      const remainingSeat = seat - 1;
 
-    // 남은 좌석수를 업데이트 해준다.
-    await this.scheduleRepository.update(
-      { id: scheduleId },
-      { availableSeat: remainingSeat },
-    );
+      // 남은 좌석수를 업데이트 해준다.
+      await manager.update(
+        Schedule,
+        { id: scheduleId },
+        { availableSeat: remainingSeat },
+      );
 
-    // 공연 가격과 남은 포인트를 비교하기
-    if (userPoint < concertPrice) {
-      throw new ForbiddenException('포인트가 부족하여 예매가 불가능합니다.');
-    }
+      // 공연 가격과 남은 포인트를 비교하기
+      if (userPoint < concertPrice) {
+        throw new ForbiddenException('포인트가 부족하여 예매가 불가능합니다.');
+      }
 
-    // 공연을 예매하면 유저가 가지고 있는 포인트 차감하기
-    const remainingPoint = userPoint - concertPrice;
+      // 공연을 예매하면 유저가 가지고 있는 포인트 차감하기
+      const remainingPoint = userPoint - concertPrice;
 
-    await this.userRepository.update({ id: userId }, { point: remainingPoint });
+      // 차감한 포인트를 db에 업데이트 해주기
+      await manager.update(User, { id: userId }, { point: remainingPoint });
 
-    const reservationConcert = await this.reservationRepository.save({
-      userId,
-      scheduleId,
+      const reservationConcert = await manager.save(Reservation, {
+        userId,
+        scheduleId,
+      });
+
+      return {
+        reservationConcert,
+        dateTime: concert.dateTime,
+        place: concert.concert.place,
+        price: concertPrice,
+      };
     });
 
-    return {
-      reservationConcert,
-      dateTime: concert.dateTime,
-      place: concert.concert.place,
-      price: concertPrice,
-    };
+    return reservation;
   }
 
   // 해당 공연의 좌석수를 가져오기 위한 함수
